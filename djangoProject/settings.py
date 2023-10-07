f48ca8d2a6988f03754a221b15d9aa2fedbafc78
vyazmetinov <ewan.vyazmetinov@gmail.com>
2023-10-07 17:40:16 +0300

    Fixed

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&&8xy$uyolw+*dz^@=^@onb(br0&-%k#u)!508o_&r*dfv-$3w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost', 'house-kirilov.vercel.app']
 
 
 
