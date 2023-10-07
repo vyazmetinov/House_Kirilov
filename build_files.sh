e8a17df04140d26d5f7c195be24d50b8777e21be
vyazmetinov <ewan.vyazmetinov@gmail.com>
2023-10-07 20:43:50 +0300

    Fixed

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
+echo "BUILD START"
 pip install -r requirenments.txt
 python 3.9 manage.py collectstatic
 pip install psycopg2-binary
+echo "BUILD END"
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -77,12 +77,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
@@ -121,10 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-
-MEDIA_URL = "img/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
