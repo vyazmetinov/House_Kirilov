7628e057a8f4590a67c783278822bac0846c51a8
vyazmetinov <ewan.vyazmetinov@gmail.com>
2023-10-07 17:20:00 +0300

    Fixed

---
+++ build_files.sh
@@ -0,0 +1,2 @@
+pip install -r requirenment.txt
+pithon 3.9 manage.py collectstatic
--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-&&8xy$uyolw+*dz^@=^@onb(br0&-%k#u)!508o_&r*dfv-$3w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -83,6 +84,7 @@ DATABASES = {
 }
 
 
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -118,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www.github.com/vyazmetinov/House_Kirilov/tree/master/static/",
-]
+MEDIA_URL = "img/"
+MEDIA_ROOT = BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
